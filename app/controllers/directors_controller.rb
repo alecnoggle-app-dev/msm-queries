ac5002187cebe0731a459b1843aed3df2e30aa8e
alecnoggle <anoggle@chicagobooth.edu>
2022-02-10 03:42:39 +0000

    Third save

--- app/controllers/directors_controller.rb
+++ app/controllers/directors_controller.rb
@@ -3,19 +3,27 @@ class DirectorsController < ApplicationController
   def index
     @list_of_directors = Director.all
 
-    render({ :template => "director_templates/index.html.erb"})
+    render({ :template => "director_templates/index.html.erb" })
   end
 
   def old
     @oldest = Director.where.not({ :dob => nil }).order({ :dob => :asc }).at(0)
     
-    render({ :template => "director_templates/old.html.erb"})
+    render({ :template => "director_templates/old.html.erb" })
   end
 
   def young
     @youngest = Director.order({ :dob => :desc }).at(0)
     
-    render({ :template => "director_templates/young.html.erb"})
+    render({ :template => "director_templates/young.html.erb" })
+  end
+
+  def director_details
+
+    @id = params.fetch("id")
+    @the_director = Director.where({ :id => @id }).at(0)
+
+    render({ :template => "director_templates/details.html.erb" })
   end
 
 end
