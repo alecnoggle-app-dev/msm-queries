1b1e746cb0db977f9e719ac3f88e6f6f29473529
alecnoggle <anoggle@chicagobooth.edu>
2022-02-10 03:08:12 +0000

    Second save

--- app/controllers/directors_controller.rb
+++ app/controllers/directors_controller.rb
@@ -1,8 +1,21 @@
 class DirectorsController < ApplicationController
 
   def index
+    @list_of_directors = Director.all
 
     render({ :template => "director_templates/index.html.erb"})
+  end
+
+  def old
+    @oldest = Director.where.not({ :dob => nil }).order({ :dob => :asc }).at(0)
+    
+    render({ :template => "director_templates/old.html.erb"})
+  end
+
+  def young
+    @youngest = Director.order({ :dob => :desc }).at(0)
+    
+    render({ :template => "director_templates/young.html.erb"})
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   get("/", { :controller => "application", :action => "homepage" })
 
   get("/directors", { :controller => "directors", :action => "index" })
+  get("/directors/eldest", { :controller => "directors", :action => "old" })
+  get("/directors/youngest", { :controller => "directors", :action => "young" })
 
 end
